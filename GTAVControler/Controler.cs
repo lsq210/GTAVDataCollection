56b48bf265854af15532ffffc331b5361bc45153
lsq <1135595767@qq.com>
2021-03-20 22:39:04 +0800

    添加项目文件。

---
+++ GTAVControler/Controler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using GTA;
+using GTA.Native;
+
+namespace GTAVControler
+{
+    class Cotroler: Script
+    {
+        public Cotroler()
+        {
+            Tick += OnTick; //刷新事件
+            KeyDown += OnKeyDown; //按键按下事件
+            Interval = 10; //刷新事件的间隔时间，单位毫秒
+        }
+
+        //刷新事件
+        void OnTick(object sender, EventArgs e)
+        {
+            //本例中不需要使用到该事件，实际如果需要刷新事件，则在刷新事件需要处理的内容写在这里
+        }
+
+        void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.T)
+            {
+                
+            }
+        }
+}
---
+++ GTAVUtils/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace GTAVUtils
+{
+    class Timer
+    {
+        public void setTimeout(String functionName, int timeout, bool autoReset = false)
+        {
+            System.Timers.Timer t = new System.Timers.Timer(timeout);//实例化Timer类，设置间隔时间为10000毫秒； 
+            t.Elapsed += new System.Timers.ElapsedEventHandler(timeup);//到达时间的时候执行事件； 
+            t.AutoReset = autoReset;//设置是执行一次（false）还是一直执行(true)； 
+            t.Enabled = true;//是否执行System.Timers.Timer.Elapsed事件；
+        }
+    }
+}
